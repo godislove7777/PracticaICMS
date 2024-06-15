9f7a5e8ef20d1de98c6a7e3bc9223d53d09eae9a
cristian <cristian_chivas-180393@hotmail.com@>
2024-06-14 22:12:21 -0600

    servicios PRACTICA IC

---
+++ src/main/java/com/example/demo/controller/InvoiceRestController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author sotobotero
+ */
+
+@RestController
+@RequestMapping("/billing")
+public class InvoiceRestController {
+
+    @GetMapping("/{id}")
+    public String get(@PathVariable String id) {
+      return "Hola "+id;
+    }
+
+
+}
